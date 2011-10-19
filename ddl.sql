2850d8d85eb49ca4dfc34680d807229af475bada
Sean M. Collins <sean@coreitpro.com>
2011-10-19 12:09:03 -0400

    Add comment table

--- ddl.sql
+++ ddl.sql
@@ -14,3 +14,14 @@ CREATE TABLE 'posts' (
 	post_text text,
 	FOREIGN KEY(user_id) REFERENCES users(id)
 );
+
+CREATE TABLE 'comments' (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	post_id INTEGER,
+	user_id INTEGER,
+	posted date,
+	published boolean,
+	comment_text text,
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(post_id) REFERENCES posts(id)
+);
